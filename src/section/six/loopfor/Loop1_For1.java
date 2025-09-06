1d795dbad655b0f79d169a3500b78b24583644c4
R!ver <77137716+the-flumen@users.noreply.github.com>
2025-09-06 14:57:17 +0900

    Revert "section5"
    This reverts commit 77cfd66683696cfaf05810db20b6e156b14d604f.

--- src/section/six/loopfor/Loop1_For1.java
+++
@@ -1,9 +0,0 @@
-package section.six.loopfor;
-
-public class Loop1_For1 {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 10; i ++) {
-            System.out.println("i = " + i);
-        }
-    }
-}
